00b6e59023e1f545329e2b8c9ce30a312c6247d4
irfanca1 <irfanchoiruddin23@gmail.com>
2023-06-08 21:56:42 +0700

    create db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
+const sequelize = require('./db/conn');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,9 +13,6 @@ const { verifyToken } = require('./middleware/VerifyToken');
 const { Register, Login, getUsers, Logout, ForgotPassword, ResetPassword } = require('./controller/UserController');
 const prefix = '/v1/api/';
 
-// Import model dan koneksi Sequelize
-const db = require('./config/database');
-
 // User API
 app.get(prefix + 'users', getUsers);
 app.post(prefix + 'register', Register);
@@ -25,13 +22,7 @@ app.post(prefix + 'forgotPassword', ForgotPassword);
 app.post(prefix + 'reset-password', ResetPassword);
 
 // Sinkronisasi model dengan database
-db.sequelize
-  .sync()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Error syncing database:', error);
-  });
+sequelize.sync();
+app.listen(5000 || process.env.PORT, () => {
+  console.log('Server Started');
+});
