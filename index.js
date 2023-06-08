a3932e8380110ea50e2bf783158486c5b1c7897d
irfanca1 <irfanchoiruddin23@gmail.com>
2023-06-08 18:47:43 +0700

    vercel

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(bodyParser.json());
 
 const { verifyToken } = require('./middleware/VerifyToken');
 const { Register, Login, getUsers, Logout, ForgotPassword, ResetPassword } = require('./controller/UserController');
 const prefix = '/v1/api/';
+
+// Import model dan koneksi Sequelize
+const db = require('./config/database');
 
 // User API
 app.get(prefix + 'users', getUsers);
@@ -22,6 +24,14 @@ app.delete(prefix + 'logout', Logout);
 app.post(prefix + 'forgotPassword', ForgotPassword);
 app.post(prefix + 'reset-password', ResetPassword);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// Sinkronisasi model dengan database
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error syncing database:', error);
+  });
