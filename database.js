4e2d3aa5d8d5c627cc88326afd20038e2617c2e1
irfanca1 <irfanchoiruddin23@gmail.com>
2023-06-09 11:32:10 +0700

    update database

--- database.js
+++ database.js
@@ -2,7 +2,6 @@ const { Sequelize } = require('sequelize');
 const pg = require('pg');
 
 const sequelize = new Sequelize(process.env.POSTGRES_URL, {
-  dialect: 'postgres',
   dialectModule: pg,
   dialectOptions: {
     ssl: {
