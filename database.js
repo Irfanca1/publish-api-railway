e05d29ccd39b18216cdca275dc050a9af581829c
irfanca1 <irfanchoiruddin23@gmail.com>
2023-06-09 15:01:32 +0700

    update database

--- database.js
+++ database.js
@@ -1,41 +1,12 @@
-const { Sequelize } = require('sequelize');
-const pg = require('pg');
-
-const { Pool } = pg;
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL + '?sslmode=require',
 });
 
-async function getPostgresConnection() {
-  const client = await pool.connect();
-  return client;
-}
-
-const sequelize = new Sequelize({
-  dialect: 'postgres',
-  dialectModule: pg,
-  define: {
-    timestamps: false,
-  },
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-
-  query: async (query, options, callback) => {
-    const client = await getPostgresConnection();
-    try {
-      const result = await client.query(query, options);
-      callback(null, result);
-    } catch (error) {
-      callback(error, null);
-    } finally {
-      client.release();
-    }
-  },
+pool.connect((err) => {
+  if (err) throw err;
+  console.log('Connect to PostgreSQL successfully!');
 });
 
-module.exports = sequelize;
+module.exports = pool;
