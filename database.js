93504ca7ca5fa07eaddd0b1bbfa40c872b3a0c9f
irfanca1 <irfanchoiruddin23@gmail.com>
2023-06-09 14:55:32 +0700

    update database

--- database.js
+++ database.js
@@ -1,13 +1,40 @@
 const { Sequelize } = require('sequelize');
 const pg = require('pg');
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + '?sslmode=require',
+});
+
+async function getPostgresConnection() {
+  const client = await pool.connect();
+  return client;
+}
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
   dialectModule: pg,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+  define: {
+    timestamps: false,
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+
+  query: async (query, options, callback) => {
+    const client = await getPostgresConnection();
+    try {
+      const result = await client.query(query, options);
+      callback(null, result);
+    } catch (error) {
+      callback(error, null);
+    } finally {
+      client.release();
+    }
   },
 });
 
