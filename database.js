efa2846c729064afd3f558bc29148451ee575e6a
irfanca1 <irfanchoiruddin23@gmail.com>
2023-06-09 11:25:49 +0700

    vercel

--- database.js
+++ database.js
@@ -1,10 +1,13 @@
 const { Sequelize } = require('sequelize');
+const pg = require('pg');
 
 const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+  dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, // Untuk mengatasi kesalahan "self signed certificate" saat menggunakan SSL
+      rejectUnauthorized: false,
     },
   },
 });
