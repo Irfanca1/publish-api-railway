8cb81cd88ef6354b2fdf9432c11c08002b3c5f6d
irfanca1 <irfanchoiruddin23@gmail.com>
2023-06-09 18:48:26 +0700

    update index.js

--- models/users.js
+++ models/users.js
@@ -1,29 +1,28 @@
-'use strict';
-const { Model } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../database');
+
+class users extends Model {
+  /**
+   * Helper method for defining associations.
+   * This method is not a part of Sequelize lifecycle.
+   * The `models/index` file will call this method automatically.
+   */
+  static associate(models) {
+    // define association here
   }
-  users.init(
-    {
-      username: DataTypes.STRING,
-      password: DataTypes.STRING,
-      nama_lengkap: DataTypes.STRING,
-      alamat: DataTypes.STRING,
-      email: DataTypes.STRING,
-      nomor_telepon: DataTypes.STRING,
-    },
-    {
-      sequelize,
-      modelName: 'users',
-    }
-  );
-  return users;
-};
+}
+users.init(
+  {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    nama_lengkap: DataTypes.STRING,
+    alamat: DataTypes.STRING,
+    email: DataTypes.STRING,
+    nomor_telepon: DataTypes.STRING,
+  },
+  {
+    sequelize,
+    modelName: 'users',
+  }
+);
+module.exports = users;
